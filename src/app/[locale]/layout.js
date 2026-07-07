ab9a46db0ee10ce4a0874ea115b060d3fbfdb0f7
Ali <aligazal.wd93@gmail.com>
2026-07-07 14:45:25 +0300

    Add Google Tap

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -2,6 +2,7 @@ import "../globals.css";
 import Header from "../components/Header";
 import localFont from "next/font/local";
 import ScrollToTop from "../components/ScrollToTop";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const fontZagel = localFont({
   src: "../../../public/fonts/ZagelArabicITF-Regular.otf",
@@ -22,6 +23,7 @@ export default async function LocaleLayout({ children, params }) {
 
   return (
     <html lang={locale} dir={dir}>
+      <GoogleTagManager gtmId="GTM-TLNGV2RL" />
       <body className={fontZagel.variable} dir={dir}>
         <ScrollToTop />
         <Header locale={locale} />
